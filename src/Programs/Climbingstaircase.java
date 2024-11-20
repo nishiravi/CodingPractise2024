d2dfea512eeeab453d0566e065c69d62eada629c
nishi <r.nishi61@gmail.com>
2024-11-20 11:59:28 -0500

    practice

--- src/Programs/Climbingstaircase.java
+++ src/Programs/Climbingstaircase.java
@@ -6,6 +6,11 @@ public class Climbingstaircase {
     public static void main(String[] args) {
 
         int n = 6;
+        int numofWays= climbStairs(n);
+        System.out.println(" number of ways to reach " + n + " stairs is  " + numofWays);
+    }
+
+    public static int climbStairs(int n) {
         int first = 1;
         int second = 2;
         for (int i = 3; i <= n; i++) {
@@ -14,6 +19,6 @@ public class Climbingstaircase {
             first = second;
             second = nthstairs;
         }
-        System.out.println(" number of ways to reach " + n + " stairs is  " + second);
+        return second;
     }
 }
