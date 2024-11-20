bb810ec2ba1e0726249104a21e6e0e877311993a
nishi <r.nishi61@gmail.com>
2024-11-20 12:43:05 -0500

    Leet Code: https://leetcode.com/problems/climbing-stairs/description/

--- src/Programs/Climbingstaircase.java
+++ src/Programs/Climbingstaircase.java
@@ -1,11 +1,11 @@
 package Programs;
 
 import java.util.ArrayList;
-
+//https://leetcode.com/problems/climbing-stairs/description/
 public class Climbingstaircase {
     public static void main(String[] args) {
 
-        int n = 6;
+        int n = 5;
         int numofWays= climbStairs(n);
         System.out.println(" number of ways to reach " + n + " stairs is  " + numofWays);
     }
@@ -13,6 +13,10 @@ public class Climbingstaircase {
     public static int climbStairs(int n) {
         int first = 1;
         int second = 2;
+        if(n==1)
+            return first;
+        else if(n==2)
+            return second;
         for (int i = 3; i <= n; i++) {
 
             int nthstairs = first + second;
