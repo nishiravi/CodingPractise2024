3086b4beb35f93cc90f696e80225f36e140c57a6
nishi <r.nishi61@gmail.com>
2025-02-07 11:38:04 -0500

    //https://leetcode.com/problems/excel-sheet-column-number/description/

---
+++ src/Programs/ExcelSheet_Leetcode.java
@@ -0,0 +1,18 @@
+package Programs;
+
+public class ExcelSheet_Leetcode {
+    //https://leetcode.com/problems/excel-sheet-column-number/description/
+    public static void main(String[] args) {
+
+        String columnTitle="A";
+        int result=0;
+        for(char c:columnTitle.toCharArray())
+        {
+            int charvalue=c-'A'+1; // gives the numeric value of the alphabet in the Excel
+            // Ex: c=A=65, 65-65+1=1;
+            // c=B=66,
+            result=result*26+charvalue;
+        }
+        System.out.println(result);
+    }
+}
