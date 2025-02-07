0263dd7ab9aa3da28a45607f2aaf598f02522a34
nishi <r.nishi61@gmail.com>
2025-02-07 14:37:08 -0500

    https://leetcode.com/problems/isomorphic-strings/description/

---
+++ src/Programs/IsomorphicStrings_Leetcode.java
@@ -0,0 +1,58 @@
+package Programs;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class IsomorphicStrings_Leetcode {
+    //https://leetcode.com/problems/isomorphic-strings/description/
+    public static void main(String[] args) {
+
+        String s="foo";
+        String t="cap";
+        System.out.println(verifyIsomorphic(s,t));
+    }
+
+    public static boolean verifyIsomorphic(String s, String t)
+    {
+        // if both string length dont match then its not Isomorphic
+        if(s.length()!=t.length())
+        {
+            return false;
+        }
+        // for mapping
+        HashMap<Character,Character> sTOt=new HashMap<>();
+        HashMap<Character,Character> tTOs=new HashMap<>();
+        //to iterate over each character of both the string
+        for(int i=0;i<s.length();i++)
+        {
+            // fetching each character from the two strings
+            char charS=s.charAt(i);
+            char charT=t.charAt(i);
+            // mapping the characters of charS with charT
+            if(sTOt.containsKey(charS))
+            {
+                if(sTOt.get(charS)!=charT)
+                {
+                    return false; // already mapping is done
+                }
+            }
+            else {
+                sTOt.put(charS,charT); // map character  s->t
+            }
+            // mapping the characters of charT with charS
+            if(tTOs.containsKey(charT))
+            {
+                if(tTOs.get(charT)!=charS)
+                {
+                    return false; // already mapping is done
+                }
+            }
+            else {
+                tTOs.put(charT,charS); // map character  s->t
+            }
+
+        }
+        return true;
+    }
+}
