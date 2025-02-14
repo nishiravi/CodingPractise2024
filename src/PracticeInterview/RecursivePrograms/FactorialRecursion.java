aa0e1a55a1f8eab498dc600f8cd839bd6ed81673
nishi <r.nishi61@gmail.com>
2025-02-14 12:30:28 -0500

    Recursion

--- src/PracticeInterview/RecursivePrograms/FactorialRecursion.java
+++ src/PracticeInterview/RecursivePrograms/FactorialRecursion.java
@@ -1,4 +1,17 @@
 package PracticeInterview.RecursivePrograms;
 
 public class FactorialRecursion {
+    public static void main(String[] args) {
+        int n=5;
+        System.out.println(fact(n));
+    }
+
+    private static int fact(int n) {
+        if (n == 0)
+            return 1;
+        else if (n == 1)
+            return 1;
+        else
+          return n*fact(n-1);
+    }
 }
--- src/PracticeInterview/RecursivePrograms/FibonaciRecusion.java
+++ src/PracticeInterview/RecursivePrograms/FibonaciRecusion.java
@@ -1,4 +1,17 @@
 package PracticeInterview.RecursivePrograms;
 
 public class FibonaciRecusion {
+    public static void main(String[] args) {
+        int n = 6;
+        System.out.println(fibonacci(n));
+    }
+
+    private static int fibonacci(int n) {
+        if (n == 0)
+            return 0;
+        else if (n == 1)
+            return 1;
+        else
+            return fibonacci(n - 1) + fibonacci(n - 2);
+    }
 }
